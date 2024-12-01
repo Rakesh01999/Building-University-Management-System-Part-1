4cf2135dbec43b20345ccfcd0cebfbdf1b7ec058
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-01 19:32:21 +0600

    user.validation -added & modified

--- first-project/src/app/modules/user/user.validation.ts
+++ first-project/src/app/modules/user/user.validation.ts
@@ -3,10 +3,7 @@ import { z } from "zod";
 const userValidationSchema = z.object({
 
     password: z.string({ invalid_type_error: 'Password must be string' }).max(20, { message: 'Password can not be more than 20 characters' }).optional(),
-    needsPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(['student', 'facaulty', 'admin']),
-    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-    isDeleted: z.boolean().optional().default(false),
+    
 });
 
 export const userValidation = {
